71bbe7d9a8b45c113005858d6328d7e1d8fcf0fb
Ja0nix <62944305+Ja0nix@users.noreply.github.com>
2021-05-04 12:34:20 +0300

    Готово:  не использовать fetch а Promise

--- js/main.js
+++ js/main.js
@@ -64,20 +64,43 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 // Переделать в ДЗ не использовать fetch а Promise
-let getRequest = (url, cb) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = () => {
+// let getRequest = (url, cb) => {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState === 4) {
+//             if (xhr.status !== 200) {
+//                 console.log('Error');
+//             } else {
+//                 cb(xhr.responseText);
+//             }
+//         }
+//     };
+//     xhr.send();
+// };
+
+let getRequest = (url, cd) =>  {
+
+    return new Promise(function(resolve, reject) {
+  
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+  
+      xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
-            if (xhr.status !== 200) {
-                console.log('Error');
+            if (xhr.status == 200) {
+            resolve(cd(xhr.responseText));
             } else {
-                cb(xhr.responseText);
+            reject('Error');
             }
         }
-    };
-    xhr.send();
-};
+      };
+ 
+      xhr.send();
+    });
+  
+  }
+
 
 // –--------------------------------
 
@@ -89,14 +112,14 @@ class ProductList {
         this._allProducts = [];
         // this._sum = 0; // Bad
 
-        // this._fetchGoods();
+        this._fetchGoods();
         // this._render();
 
-        this._getProducts()
-            .then((data) => {
-                this._goods = data;
-                this._render();
-            });
+        // this._getProducts()
+        //     .then((data) => {
+        //         this._goods = data;
+        //         this._render();
+        //     });
     }
 
     sum() {
@@ -107,21 +130,22 @@ class ProductList {
         // return this._goods.reduce((sum, { price }) => sum + price, 0); // Good
     }
 
-    // _fetchGoods() {
-    //     getRequest(`${API}/catalogData.json`, (data) => {
-    //         this._goods = JSON.parse(data);
-    //         console.log(this._goods);
-    //         this._render();
-    //     });
-    // }
-
-    _getProducts() {
-        return fetch(`${API}/catalogData.json`)
-            .then((response) => response.json())
-            .catch((error) => {
-                console.log(error);
-            });
+    _fetchGoods() {
+        getRequest(`${API}/catalogData.json`, (data) => {
+            this._goods = JSON.parse(data);
+            // console.log(this._goods);
+            this._render();
+        });
     }
+
+    // _getProducts() {
+    //     return fetch(`${API}/catalogData.json`)
+    //         .then((response) => response.json())
+    //         .catch((error) => {
+    //             console.log(error);
+    //         });
+        
+    // }
 
     _render() {
         const block = document.querySelector(this.container);
