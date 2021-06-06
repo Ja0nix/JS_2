f54e673e62fa75d644f9a1654f1ee69585f658cb
Ja0nix <62944305+Ja0nix@users.noreply.github.com>
2021-06-06 13:58:59 +0300

    Вывод каталога на главной
    Выведен каталог на главной из json файла, нужные стили применены.
    В работе: кнопка добавления в корзину товара из каталога.

--- fproject/public/js/ProducComp.js
+++ fproject/public/js/ProducComp.js
@@ -32,7 +32,9 @@ Vue.component('products', {
     },
     template: `
         <div class="products">
-            <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+            <div class="featured__items">
+                <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+            </div>
         </div>
     `
 });
@@ -40,20 +42,20 @@ Vue.component('product', {
     props: ['product', 'img'],
     template: `
     <div class="product-item">
-                <img :src="img" alt="Some img">
+    <!-- <img :src="img" alt="Some img">
                 <div class="desc">
                     <h3>{{product.product_name}}</h3>
                     <p>{{product.price}}₽</p>
-                         <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>
+                         <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>-->
 <!-- 2                    <button class="buy-btn" @click="$parent.$parent.$refs.cart.addProduct(product)">Купить</button>-->
-                </div>
+<!--  </div>-->
                 <div class="featured__item">
                     <a href="#"></a><img :src=product.img :alt=product.product_name class="featured__img"></a>
-                    <a href="#"><img src="img/featured/hover.png" alt="" class="featured__img-hover"></a>
+                    <a @click="$root.$refs.cart.addProduct(product)"><img src="img/featured/hover.png" alt="" class="featured__img-hover"></a>
                     <div class="featured__description">
                         <a href="product.html"><h3 class="featured__h3">{{product.product_name}}</h3></a>
                         <p class="featured__p">{{product.product_description}}</p>
-                        <p class="featured__price">{{product.price}}</p>
+                        <p class="featured__price">\${{product.price}}</p>
                     </div>
                 </div>
             </div>
